fd3a68b8f00a9f6228e3628f5ac51660196f1263
Murilo Santos <54122749+muriloSantos12@users.noreply.github.com>
2020-10-29 19:47:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE stocks
 (date text, trans text, symbol text, qty real, price real)''')
 
-c.execute("INSERT INTO stocks VALUES
-('2006-01-05','BUY','RHAT',100,35.14)")
+c.execute("INSERT INTO stocks VALUES('2006-01-05','BUY','RHAT',100,35.14)")
 
 conn.commit()
 conn.close()
